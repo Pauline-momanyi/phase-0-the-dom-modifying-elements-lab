9ed180a3a19708c7c2e9c85bcbef81aef092e1d0
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-11 09:47:51 +0300

    Modify elements in the DOM

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+document.querySelector('main').remove()
+let newh = document.createElement('h1')
+//newh.id = 'victory'
+let newHeader = document.querySelector('body').appendChild(newh)
+newHeader.id = 'victory'
+newHeader.innerHTML= "PAULINE is the champion"
+
+
